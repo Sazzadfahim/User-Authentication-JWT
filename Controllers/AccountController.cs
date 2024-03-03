6bebd0af396d4fcebb8a6031e059bf223dc4b113
Sazzad Hossain <ishn@liveimplevista.onmicrosoft.com>
2024-03-03 17:47:07 +0600

    Day End

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using UserAuthentication.Models;
+
+namespace UserAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private readonly IConfiguration _configuration;
+
+        public AccountController(UserManager<User> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            
+        }
+    }
+}
--- Data/AppDBContext.cs
+++ Data/AppDBContext.cs
@@ -12,6 +12,6 @@ namespace UserAuthentication.Data
             
         }   
 
-        public DbSet<User> Users { get; set; }
+        //public DbSet<User> Users { get; set; }
     }
 }
